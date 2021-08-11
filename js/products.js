3d85bb8dc25c66da921dea1abe34da721afba967
marialaura-13 <88438358+marialaura-13@users.noreply.github.com>
2021-08-11 09:35:18 -0300

    1

--- js/products.js
+++ js/products.js
@@ -1,6 +1,48 @@
+var categoriesArray = [];
+
+function showCategoriesList(array){
+
+    var htmlContentToAppend = "";
+    for(var i = 0; i < array.length; i++){
+        var category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" title="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <small class="text-muted">` + category.soldCount + ` artículos</small>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `;
+        //console.log(htmlContentToAppend);
+
+       
+    }
+    document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
 
+                console.log(categoriesArray);
+
+
+            showCategoriesList(categoriesArray);
+        }
+    });
 });
